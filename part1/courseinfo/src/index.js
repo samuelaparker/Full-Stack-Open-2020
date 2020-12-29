c892ac57c1f59e60263f33de4720e686aa410353
samuelaparker <samuel.a.parker00@gmail.com>
2020-12-29 11:45:51 -0500

    1.5 11:45am

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -6,7 +6,7 @@ const Header = (props) => {
 
   return (
     <div>
-      <h1>{props.course}</h1>
+      <h1>{props.courseName}</h1>
     </div>
   )
 }
@@ -40,28 +40,28 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
