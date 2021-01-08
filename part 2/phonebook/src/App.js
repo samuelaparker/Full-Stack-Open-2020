486c9af52542e3fd2139c0ccd8ba5b32c4604b43
samuelaparker <samuel.a.parker00@gmail.com>
2021-01-08 10:29:58 -0500

    1.8.21 ex 2.8

--- part 2/phonebook/src/App.js
+++ part 2/phonebook/src/App.js
@@ -1,33 +1,50 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '555-2345' }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNumber] = useState('')
+
 
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
-    setPersons(persons.concat(personObject))
-    setNewName('')
+    if (persons.some(n => n.name === newName) || persons.some(n =>n.number === newNumber)) {
+      alert(`${newName} ${newNumber} is already added to the phonebook`)
+    } else {
+      setPersons(persons.concat(personObject))
+      setNewName('')
+      setNumber('')
+    }
   }
-
-  const handleInput = (event) => {
-    console.log(event.target.value)
+  
+  const handleNameInput = (event) => {
     setNewName(event.target.value)
   }
- 
+  const handleNumberInput = (event) => {
+    setNumber(event.target.value)
+  }
+   
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson} >
         <div>
-          name: <input 
-          value={newName} 
-          onChange={handleInput}
+          name: <input
+            value={newName}
+            onChange={handleNameInput}
+          />
+        </div>
+        <div>
+          number: <input
+            value={newNumber}
+            onChange={handleNumberInput}
           />
         </div>
         <div>
@@ -35,7 +52,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(n => <p key={n.name}>{n.name}</p>)}
+      {persons.map(n => <p key={n.name}>{n.name} {n.number} </p>)}
     </div>
   )
 }
