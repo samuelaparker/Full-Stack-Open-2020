7d779bfb178d148c451d0ea035c43d9bf61f7d3e
samuelaparker <samuel.a.parker00@gmail.com>
2021-01-11 21:13:57 -0500

    ex 2.12

---
+++ part 2/countries/src/App.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [searchInput, setSearchInput] = useState('')
+
+  const handleSearchInput = (event) => {
+    setSearchInput(event.target.value)
+  }
+
+  useEffect(() => {
+    axios
+      .get(`https://restcountries.eu/rest/v2/all`)
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
+  const filteredCountries = countries.map(n => n).filter(n => n.name.toLocaleLowerCase().includes(searchInput.toLocaleLowerCase()))
+
+  return (
+    <div>
+      <Search handleSearchInput={handleSearchInput} searchInput={searchInput} />
+      <RenderData filteredCountries={filteredCountries} />
+    </div>
+  )
+}
+
+const Search = (props) => {
+  return (
+    <div>Find countries:
+      <input
+        value={props.searchInput}
+        onChange={props.handleSearchInput}
+      />
+    </div>
+  )
+}
+
+const RenderData = ({ filteredCountries }) => {
+  console.log(filteredCountries)
+  if (filteredCountries.length > 10) {
+    return (
+      <div>
+        Too many matches, specify another filter.
+      </div>
+    )
+
+  } else if (filteredCountries.length < 10 && filteredCountries.length >= 2) {
+    return (
+      <div>
+        {filteredCountries.map((n) => <p key={n.numericCode}>{n.name} </p>)}
+      </div>
+    )
+  }
+  else
+    return (
+      <div>
+        {filteredCountries.map(({ name, capital, population, languages, flag, numericCode }) =>
+          <div key={numericCode}>
+            <h1>{name}</h1>
+            <p>Capital: {capital}</p>
+            <p>Population: {population}</p>
+            <h2>Languages</h2>
+            <div>
+              {languages.map(n => <li key={n.so639_1}>{n.name}</li>)}
+            </div>
+            <div>
+              <img src={flag} alt="Flag" style={{ maxWidth: "250px", marginTop: "25px" }} />
+            </div>
+          </div>
+        )
+        }
+      </div>
+    )
+}
+
+export default App;
