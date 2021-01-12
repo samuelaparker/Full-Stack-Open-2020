7530451366a44f55b926f01d676063069b0868e7
samuelaparker <samuel.a.parker00@gmail.com>
2021-01-11 22:42:40 -0500

    10:42

--- part 2/countries/src/App.js
+++ part 2/countries/src/App.js
@@ -36,24 +36,8 @@ const Search = (props) => {
     </div>
   )
 }
-
-const RenderData = ({ filteredCountries }) => {
+const ShowInfo = ({filteredCountries}) => {
   console.log(filteredCountries)
-  if (filteredCountries.length > 10) {
-    return (
-      <div>
-        Too many matches, specify another filter.
-      </div>
-    )
-
-  } else if (filteredCountries.length < 10 && filteredCountries.length >= 2) {
-    return (
-      <div>
-        {filteredCountries.map((n) => <p key={n.numericCode}>{n.name} </p>)}
-      </div>
-    )
-  }
-  else
     return (
       <div>
         {filteredCountries.map(({ name, capital, population, languages, flag, numericCode }) =>
@@ -64,6 +48,63 @@ const RenderData = ({ filteredCountries }) => {
             <h2>Languages</h2>
             <div>
               {languages.map(n => <li key={n.so639_1}>{n.name}</li>)}
+            </div>
+            <div>
+              <img src={flag} alt="Flag" style={{ maxWidth: "250px", marginTop: "25px" }} />
+            </div>
+          </div>
+        )
+        }
+        {}
+
+
+      </div>
+    )
+  }
+
+const RenderData = ({ filteredCountries }) => {
+  const [clickToggle, setClickToggle] = useState(false)
+  
+  const triggerShowInfo = () => {
+    setClickToggle(true)
+    console.log(clickToggle)
+  }
+  
+  if (filteredCountries.length > 10) {
+    return (
+      <div>
+        Too many matches, specify another filter.
+      </div>
+    )
+
+  } else if (filteredCountries.length < 10 && filteredCountries.length >= 2) {
+    return (
+      <div>
+        {filteredCountries.map((n) => 
+        <div key={n.numericCode}>
+        <p style={{display : 'inline'}} key={n.numericCode}>{n.name} </p>
+        <button onClick={triggerShowInfo}>show</button>
+        </div>
+        )
+        }
+        {clickToggle === true &&
+        <ShowInfo filteredCountries={filteredCountries} />
+        }
+      </div>
+    ) 
+  } 
+
+  else
+    return (
+      <div>
+        {filteredCountries.map(({ name, capital, population, languages, flag, numericCode }) =>
+          <div key={numericCode}>
+            <h1>{name}</h1>
+            <p>Capital: {capital}</p>
+            <p>Population: {population}</p>
+            <h2>Languages</h2>
+            <div>
+              {languages.map((n, i) => <li key={i}>{n.name}</li>)}
             </div>
             <div>
               <img src={flag} alt="Flag" style={{ maxWidth: "250px", marginTop: "25px" }} />
