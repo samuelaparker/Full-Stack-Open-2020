d9de987e6e84a5e51e1df0d56ebf59424c4f2999
samuelaparker <samuel.a.parker00@gmail.com>
2021-01-12 22:15:04 -0500

    10:14

--- part 2/countries/src/App.js
+++ part 2/countries/src/App.js
@@ -2,26 +2,62 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const App = () => {
+
+
   const [countries, setCountries] = useState([])
   const [searchInput, setSearchInput] = useState('')
+  const [weather, setWeather] = useState([])
+  const [query, setQuery] = useState('')
+
+  const filteredCountries = countries.map(n => n).filter(n => n.name.toLocaleLowerCase().includes(searchInput.toLocaleLowerCase()))
 
   const handleSearchInput = (event) => {
     setSearchInput(event.target.value)
+    const filteredCapital = filteredCountries.map(n => n.capital)
+    // if (filteredCapital.length === 1) {
+    //   setQuery(filteredCapital)
+    //   getWeatherData(query)
+    //   console.log(filteredCountries)
+    // } else {
+    //   setQuery([])
+    // }
+    setQuery()
   }
+  console.log(countries)
 
   useEffect(() => {
     axios
       .get(`https://restcountries.eu/rest/v2/all`)
-      .then(response => {
+      .then(response =>
         setCountries(response.data)
-      })
+      )
   }, [])
-  const filteredCountries = countries.map(n => n).filter(n => n.name.toLocaleLowerCase().includes(searchInput.toLocaleLowerCase()))
+
+  const getWeatherData = (query) => {
+    const api_key = process.env.REACT_APP_API_KEY
+    axios
+      .get(`http://api.weatherstack.com/current`, {
+        params: {
+          access_key: api_key,
+          query: query.toString(),
+          units: `f`
+        }
+      })
+      .then(response => {
+
+        setWeather(response.data)
+
+      }).catch(error => {
+        setWeather([])
+      })
+  }
+  console.log(weather)
 
   return (
     <div>
       <Search handleSearchInput={handleSearchInput} searchInput={searchInput} />
-      <RenderData filteredCountries={filteredCountries} />
+      <RenderData filteredCountries={filteredCountries} weather={weather} />
+      <Weather weather={weather} />
     </div>
   )
 }
@@ -36,38 +72,18 @@ const Search = (props) => {
     </div>
   )
 }
-const ShowInfo = ({filteredCountries, buttonId}) => {
-  console.log(filteredCountries[buttonId].capital)
-  console.log(buttonId)
-  const country = filteredCountries[buttonId]
-    return (
-      <div>
-          <div key={country.numericCode}>
-            <h1>{country.name}</h1>
-            <p>Capital: {country.capital}</p>
-            <p>Population: {country.population}</p>
-            <h2>Languages</h2>
-            <div>
-              {country.languages.map((n, i) => <li key={i}>{n.name}</li>)}
-            </div>
-            <div>
-              <img src={country.flag} alt="Flag" style={{ maxWidth: "250px", marginTop: "25px" }} />
-            </div>
-          </div>
-      </div>
-    )
-  }
 
-const RenderData = ({ filteredCountries }) => {
+
+const RenderData = ({ filteredCountries, weather }) => {
   const [clickToggle, setClickToggle] = useState(false)
   const [buttonId, setButtonId] = useState(0)
-  
+
   const triggerShowInfo = (event) => {
     console.log(event.target.id)
     setClickToggle(true)
     setButtonId(event.target.id)
   }
-  
+
   if (filteredCountries.length > 10) {
     return (
       <div>
@@ -78,22 +94,23 @@ const RenderData = ({ filteredCountries }) => {
   } else if (filteredCountries.length < 10 && filteredCountries.length >= 2) {
     return (
       <div>
-        {filteredCountries.map((n, i) => 
-        <div key={n.numericCode}>
-        <p style={{display : 'inline'}} key={n.numericCode}>{n.name} </p>
-        <button id={i} onClick={triggerShowInfo}>show</button>
-        </div>
+        {filteredCountries.map((n, i) =>
+          <div key={n.numericCode}>
+            <p style={{ display: 'inline' }} key={n.numericCode}>{n.name} </p>
+            <button id={i} onClick={triggerShowInfo}>show</button>
+          </div>
         )
         }
         {clickToggle === true &&
-        <ShowInfo filteredCountries={filteredCountries} buttonId={buttonId} />
+          <ShowInfo filteredCountries={filteredCountries} buttonId={buttonId} />
         }
       </div>
-    ) 
-  } 
+    )
+  }
 
   else
     return (
+
       <div>
         {filteredCountries.map(({ name, capital, population, languages, flag, numericCode }) =>
           <div key={numericCode}>
@@ -110,8 +127,44 @@ const RenderData = ({ filteredCountries }) => {
           </div>
         )
         }
+
       </div>
     )
+}
+
+const ShowInfo = ({ filteredCountries, buttonId }) => {
+  console.log(filteredCountries[buttonId].capital)
+  console.log(buttonId)
+  const country = filteredCountries[buttonId]
+  return (
+    <div>
+      <div key={country.numericCode}>
+        <h1>{country.name}</h1>
+        <p>Capital: {country.capital}</p>
+        <p>Population: {country.population}</p>
+        <h2>Languages</h2>
+        <div>
+          {country.languages.map((n, i) => <li key={i}>{n.name}</li>)}
+        </div>
+        <div>
+          <img src={country.flag} alt="Flag" style={{ maxWidth: "250px", marginTop: "25px" }} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const Weather = ({ weather }) => {
+
+  return (
+    <div>
+      {/* <div>{weather}</div>
+      <h3>Weather in: {weather.location.name}</h3>
+      <h4>temperature: {weather.current.temperature} </h4>
+      <img src={weather.current.weather_icons[0]} alt="weather icon" style={{ maxWidth: "100px", marginTop: "25px" }} />
+      <h4>Wind: {weather.current.wind_speed} mph Direction: {weather.current.wind_dir}</h4> */}
+    </div>
+  )
 }
 
 export default App;
