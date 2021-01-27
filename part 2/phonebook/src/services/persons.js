cf6dd9e4ffb546c365ce93e4e7704cb1846ff9ca
samuelaparker <samuel.a.parker00@gmail.com>
2021-01-26 20:30:43 -0500

    ex 3.9

--- part 2/phonebook/src/services/persons.js
+++ part 2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://murmuring-everglades-92656.herokuapp.com/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
