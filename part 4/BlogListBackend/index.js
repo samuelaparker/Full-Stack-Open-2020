50e718a1c84f879aeece6d8db76dc9a8a7fce5ad
samuelaparker <samuel.a.parker00@gmail.com>
2021-02-09 15:50:15 -0500

    refactor step 2 (routing)

--- part 4/BlogListBackend/index.js
+++ part 4/BlogListBackend/index.js
@@ -2,15 +2,10 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
+
 const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+
 
 const Blog = mongoose.model('Blog', blogSchema)
 
@@ -20,23 +15,6 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, us
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = 3003
 app.listen(PORT, () => {
