e0b362d9849bd6c0ddf06b2761ec1ffd2ea265bf
samuelaparker <samuel.a.parker00@gmail.com>
2021-02-24 11:19:59 -0500

    11:19

--- part 4/BlogListBackend/tests/blog_api.test.js
+++ part 4/BlogListBackend/tests/blog_api.test.js
@@ -4,6 +4,17 @@ const app = require('../app')
 
 const api = supertest(app)
 
+// let users = async () => {
+//     const userList = await api.get('/api/blogs')
+//     return userList.map(n => n.toJSON())
+// }
+
+// console.log(users())
+
+// test('', async () => {
+//     expect(users())
+// })
+
 test('notes are returned as json', async () => {
   await api
     .get('/api/blogs')
@@ -14,6 +25,10 @@ test('check that all blogs are returned', async () => {
     const response = await api.get('/api/blogs')
     expect(response.body).toHaveLength(response.body.length)
   })
+//   test('check if blogs have id property', async () => {
+//       const response = await api.get('/api/blogs')
+//     expect(fetchNewFlavorIdea()).toBeDefined();
+//   });
 
 afterAll(() => {
   mongoose.connection.close()
