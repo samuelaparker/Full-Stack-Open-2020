0ea6fcb53ba1b04d3a2701a0c12de5c6520fcfe4
samuelaparker <samuel.a.parker00@gmail.com>
2021-02-24 10:35:48 -0500

    ex. 4.8

--- part 4/BlogListBackend/tests/blog_api.test.js
+++ part 4/BlogListBackend/tests/blog_api.test.js
@@ -1,1 +1,20 @@
 const supertest = require('supertest')
+const mongoose = require('mongoose')
+const app = require('../app')
+
+const api = supertest(app)
+
+test('notes are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+test('check that all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(response.body.length)
+  })
+
+afterAll(() => {
+  mongoose.connection.close()
+})
