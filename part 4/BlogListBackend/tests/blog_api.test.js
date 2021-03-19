5b889f6bf31e0aa3c8defcb05e9f81c6f2dcb078
samuelaparker <samuel.a.parker00@gmail.com>
2021-03-18 23:50:04 -0400

    ex. 4.10

--- part 4/BlogListBackend/tests/blog_api.test.js
+++ part 4/BlogListBackend/tests/blog_api.test.js
@@ -4,16 +4,29 @@ const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
 
-// let users = async () => {
-//     const userList = await api.get('/api/blogs')
-//     return userList.map(n => n.toJSON())
-// }
+const initialBlogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+  },
+]
 
-// console.log(users())
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
 
-// test('', async () => {
-//     expect(users())
-// })
 
 test('notes are returned as json', async () => {
   await api
@@ -23,17 +36,35 @@ test('notes are returned as json', async () => {
 })
 test('check that all blogs are returned', async () => {
     const response = await api.get('/api/blogs')
-    expect(response.body).toHaveLength(response.body.length)
-    console.log(response.body)
+    expect(response.body.length).toBe(initialBlogs.length)
   })
 
 test('check if blogs have id property', async () => {
   const result = await api.get('/api/blogs')
-  let arr = result.body.map(n => n)
-  console.log(arr)
- 
   expect(result.body.map(blog => blog.id)).toBeDefined()
 });
+
+test('a valid note can be added', async () => {
+  const newPost = {
+    title: 'Building Good Study Habits',
+    author: 'Mathew Salavitch',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newPost)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const title = response.body.map(r => r.title)
+
+  expect(response.body).toHaveLength(initialBlogs.length + 1)
+  expect(title).toContain('Building Good Study Habits')
+})
 
 afterAll(() => {
   mongoose.connection.close()
