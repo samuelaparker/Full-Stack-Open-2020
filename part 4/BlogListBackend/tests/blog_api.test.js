9dbc99240c887dcdaf62f70550d529e434e77369
samuelaparker <samuel.a.parker00@gmail.com>
2021-04-02 15:37:47 -0400

    format

--- part 4/BlogListBackend/tests/blog_api.test.js
+++ part 4/BlogListBackend/tests/blog_api.test.js
@@ -2,28 +2,20 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
+const helper = require('./test_helper')
 const Blog = require('../models/blog')
 
-const helper = require('./test_helper')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
+
 
 describe('when there is initially some blogs saved', () => {
-  beforeEach(async () => {
-    await Blog.deleteMany({})
-
-    for (let blog of helper.initialBlogs) {
-      let blogObject = new Blog(blog)
-      await blogObject.save()
-    }
-  })
-
-  // beforeEach(async () => {
-  //   await Blog.deleteMany({})
-  //   let blogObject = new Blog(initialBlogs[0])
-  //   await blogObject.save()
-  //   blogObject = new Blog(initialBlogs[1])
-  //   await blogObject.save()
-  // })
-
 
   test('blogs are returned as json', async () => {
     await api
@@ -34,69 +26,74 @@ describe('when there is initially some blogs saved', () => {
   test('check that all blogs are returned', async () => {
     const response = await api.get('/api/blogs')
     expect(response.body.length).toBe(helper.initialBlogs.length)
+  })    
+})
+
+describe('viewing a specific note', () => {
+
+  test('check if blogs have id property', async () => {
+    const result = await api.get('/api/blogs')
+    expect(result.body.map(blog => blog.id)).toBeDefined()
   })
 
-})
-
-test('check if blogs have id property', async () => {
-  const result = await api.get('/api/blogs')
-  expect(result.body.map(blog => blog.id)).toBeDefined()
-})
-
-
-test('a valid blog can be added', async () => {
-  const newPost = {
-    title: 'Building Good Study Habits',
-    author: 'Mathew Salavitch',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newPost)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  const response = await api.get('/api/blogs')
-
-  const title = response.body.map(r => r.title)
-
-
-  expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
-  expect(title).toContain('Building Good Study Habits')
-})
-
-test('when blog\'s likes is not set it will be set to 0', async () => {
-  const newPost = {
-    title: 'Look at this!',
-    author: 'Dingus Mcgee',
-    url: 'https://github.com/samuelaparker',
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newPost)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  const blogNoLikes = await Blog.findOne({
-    title: 'Look at this!',
-    author: 'Dingus Mcgee',
+  test('a specific note can be viewed', async () => {
+    const notesAtStart = await helper.blogsInDb()
+    const noteToView = notesAtStart[0]
+    const resultNote = await api
+      .get(`/api/blogs/${noteToView.id}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    const processedNoteToView = JSON.parse(JSON.stringify(noteToView))
+    expect(resultNote.body).toEqual(processedNoteToView)
   })
-  expect(blogNoLikes.likes).toBe(0)
 })
 
-test('fails with statuscode 400 id is invalid', async () => {
-  const invalidId = '5a3d5da59070081a82a3445'
+describe('addition of a new note', () => {
 
-  await api
-    .get(`/api/blogs/${invalidId}`)
-    .expect(400)
-    
-})
+  test('a valid blog can be added', async () => {
+    const newPost = {
+      title: 'Building Good Study Habits',
+      author: 'Mathew Salavitch',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+    }
+    await api
+      .post('/api/blogs')
+      .send(newPost)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    const response = await api.get('/api/blogs')
+    const title = response.body.map(r => r.title)
+    expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+    expect(title).toContain('Building Good Study Habits')
+  })
 
-test('fails with status code 400 if data is invalid', async () => {
+  test('when blog\'s likes is not set it will be set to 0', async () => {
+    const newPost = {
+      title: 'Look at this!',
+      author: 'Dingus Mcgee',
+      url: 'https://github.com/samuelaparker',
+    }
+    await api
+      .post('/api/blogs')
+      .send(newPost)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    const blogNoLikes = await Blog.findOne({
+      title: 'Look at this!',
+      author: 'Dingus Mcgee',
+    })
+    expect(blogNoLikes.likes).toBe(0)
+  })
+
+  test('fails with statuscode 400 id is invalid', async () => {
+    const invalidId = '5a3d5da59070081a82a3445'
+    await api
+      .get(`/api/blogs/${invalidId}`)
+      .expect(400)
+  })
+
+  test('fails with status code 400 if data is invalid', async () => {
     const newBlogNoUrl = {
       title: 'Type wars',
       author: 'Robert C. Martin',
@@ -105,32 +102,16 @@ test('fails with status code 400 if data is invalid', async () => {
       author: 'Robert C. Martin',
       url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
     }
-  
-    await api.post('/api/blogs').send(newBlogNoUrl)
+    await api.post('/api/blogs')
+      .send(newBlogNoUrl)
       .expect(400)
-  
-    await api.post('/api/blogs').send(newBlogNoTitle)
+    await api.post('/api/blogs')
+      .send(newBlogNoTitle)
       .expect(400)
-  
     const blogsAtDb = await helper.blogsInDb()
-    console.log(blogsAtDb)
-    expect(blogsAtDb.length).toBe(helper.initialBlogs.length + 2)
+    expect(blogsAtDb.length).toBe(helper.initialBlogs.length)
   })
-
-test('a specific note can be viewed', async () => {
-  const notesAtStart = await helper.blogsInDb()
-
-  const noteToView = notesAtStart[0]
-
-  const resultNote = await api
-    .get(`/api/blogs/${noteToView.id}`)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  const processedNoteToView = JSON.parse(JSON.stringify(noteToView))
-  expect(resultNote.body).toEqual(processedNoteToView)
 })
-
 
 
 afterAll(() => {
