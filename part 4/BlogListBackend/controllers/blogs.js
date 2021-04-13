86c037706049e0b9f4a2cca7d750c4c2ca7d0999
samuelaparker <samuel.a.parker00@gmail.com>
2021-04-12 22:14:07 -0400

    ex 4.18

--- part 4/BlogListBackend/controllers/blogs.js
+++ part 4/BlogListBackend/controllers/blogs.js
@@ -3,7 +3,15 @@ const blogsRouter = require('express').Router()
 //Models are responsible for creating and reading documents from the underlying MongoDB database.
 const User = require('../models/user')
 const Blog = require('../models/blog')
+const jwt = require('jsonwebtoken')
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -16,9 +24,14 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
   
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
   // console.log('this is body', body)
   //blog (lowercase) is a document (an instance of a model)
-  const user = await User.findById(body.id)
 
   console.log('!!!!!!!!!!', user._id)
 
